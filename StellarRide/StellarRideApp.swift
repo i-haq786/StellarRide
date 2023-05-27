a13ff7835ad51741775761aa4259a264fb75cc9a
Inzamam <i.haqcs@gmail.com>
2023-05-28 02:02:25 +0530

    Initial Commit

---
+++ StellarRide/StellarRideApp.swift
@@ -0,0 +1,17 @@
+//
+//  StellarRideApp.swift
+//  StellarRide
+//
+//  Created by Inzamam on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct StellarRideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
